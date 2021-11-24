7879c4dd0231874ab6c708cac4df0d24afce0dfc
7268026802 <89690738+7268026802@users.noreply.github.com>
2021-11-24 20:06:19 +0530

    java program

---
+++ src/ForthQuestion.java
@@ -0,0 +1,13 @@
+public class ForthQuestion {
+    String s1;
+    String s2;
+
+    public static void main(String[] args) {
+        ForthQuestion obj1=new ForthQuestion();
+        obj1.s1=args[0];
+        obj1.s2=args[1];
+        System.out.println(obj1.s1);
+        System.out.println(obj1.s2);
+
+    }
+}
